d958c95910a08346313f5d54fb88d3e546222cff
mizuyabu <b3176@oic.jp>
2016-11-23 16:50:17 +0900

    <create>websocket通信による位置情報更新処理作成

--- app/src/main/java/com/om1/stamp_rally/controller/SampleGPSActivity.java
+++ app/src/main/java/com/om1/stamp_rally/controller/SampleGPSActivity.java
@@ -10,20 +10,30 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.neovisionaries.ws.client.WebSocket;
+import com.neovisionaries.ws.client.WebSocketAdapter;
+import com.neovisionaries.ws.client.WebSocketException;
+import com.neovisionaries.ws.client.WebSocketFactory;
 import com.om1.stamp_rally.R;
 
-import java.util.Random;
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import java.io.IOException;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
-import butterknife.OnClick;
 
 public class SampleGPSActivity extends AppCompatActivity{
     private LocationManager mLocationManager;
     private Location location;
     private final String providerName = LocationManager.GPS_PROVIDER;
+    private final EventBus eventBus = EventBus.getDefault();
+
 
     @InjectView(R.id.latitudeTextView)
     TextView latitudeTextView;
@@ -35,6 +45,7 @@ public class SampleGPSActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_gps);
         ButterKnife.inject(this);
+        eventBus.register(this);
 
         mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
@@ -46,23 +57,54 @@ public class SampleGPSActivity extends AppCompatActivity{
         location.setAccuracy(Criteria.ACCURACY_HIGH);
         location.setTime(System.currentTimeMillis());
         location.setElapsedRealtimeNanos(System.currentTimeMillis());
+
+        connectMockLocation();
     }
 
     @Override
     protected void onStop() {
+        eventBus.unregister(this);
         super.onStop();
     }
 
-    @OnClick(R.id.GpsUpdateButton)
-    void updateGps(){
-        Random rnd = new Random();
-        location.setLatitude(rnd.nextDouble());
-        location.setLongitude(rnd.nextDouble());
+    private void connectMockLocation(){
+        new Thread(){
+            @Override
+            public void run(){
+                try {
+                    new WebSocketFactory().createSocket("ws://10.0.2.2:8080/stamp-rally/location", 5000)
+                            .connect()
+                            .addListener(new WebSocketAdapter() {
+                                @Override
+                                public void onTextMessage(WebSocket websocket, String message)
+                                        throws Exception {
+                                    Log.d("onTextMessage", message);
+                                    String[] location = message.split(",", 0);
+                                    double latitude = Double.valueOf(location[0]);
+                                    double longitude = Double.valueOf(location[1]);
+                                    eventBus.post(new LocationEvent(latitude, longitude));
+                                }
+                            });
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                } catch (WebSocketException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    void updateGps(LocationEvent event){
+        Log.d("GPS", "updateGps");
+        location.setLatitude(event.getLatitude());
+        location.setLongitude(event.getLongitude());
         mLocationManager.setTestProviderLocation(providerName, location);
+
         mLocationManager.requestLocationUpdates(
-                providerName, //LocationManager.GPS_PROVIDER, //LocationManager.NETWORK_PROVIDER,
-                1000, // 通知のための最小時間間隔（ミリ秒）
-                3, // 通知のための最小距離間隔（メートル）
+                providerName,
+                1000,
+                3,
                 new LocationListener() {
                     @Override
                     public void onLocationChanged(Location location) {
@@ -85,5 +127,20 @@ public class SampleGPSActivity extends AppCompatActivity{
                     }
                 }
         );
+    }
+
+    private class LocationEvent{
+        private double latitude;
+        private double longitude;
+        public LocationEvent(double latitude, double longitude){
+            this.latitude = latitude;
+            this.longitude = longitude;
+        }
+        public double getLatitude(){
+            return latitude;
+        }
+        public double getLongitude(){
+            return longitude;
+        }
     }
 }
