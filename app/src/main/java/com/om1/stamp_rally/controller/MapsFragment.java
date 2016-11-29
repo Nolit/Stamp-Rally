2bf2f25f6368434bd4ec73798e0f68d7ca4b9b0f
Nolit <yaboonjapan@gmail.com>
2016-11-29 23:31:27 +0900

    <update>不要な依存関係削除

--- app/src/main/java/com/om1/stamp_rally/controller/MapsFragment.java
+++ app/src/main/java/com/om1/stamp_rally/controller/MapsFragment.java
@@ -32,11 +32,17 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+
 
 public class MapsFragment extends Fragment implements LocationListener,OnMapReadyCallback {
+    private static final int CAN_STAMP_METER= 50;
 
     private GoogleMap mMap;
     private LocationManager mLocationManager;
+    @InjectView(R.id.cameraIcon)
     ImageButton cameraIcon;
 
     //状態別マーカーの宣言
@@ -64,13 +70,12 @@ public class MapsFragment extends Fragment implements LocationListener,OnMapRead
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        ButterKnife.inject(this, view);
 
         FragmentManager fm = getChildFragmentManager();
         SupportMapFragment mapFragment = (SupportMapFragment)fm.findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        //カメラボタンの透明化
-        cameraIcon = (ImageButton) view.findViewById(R.id.cameraIcon);
         cameraIcon.setVisibility(View.INVISIBLE);
 
         //状態別マーカーの設定
@@ -92,22 +97,6 @@ public class MapsFragment extends Fragment implements LocationListener,OnMapRead
         LatLng start_position = new LatLng(21.308889,-157.826111);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(start_position));
 
-        //プロバイダの取得
-
-
-        Criteria criteria = new Criteria();
-        criteria.setAccuracy(Criteria.ACCURACY_FINE);       //高精度
-        criteria.setAccuracy(Criteria.NO_REQUIREMENT);
-
-        String provider = mLocationManager.getBestProvider(criteria, true);
-
-        if(mLocationManager.isProviderEnabled(provider)){
-            System.out.println("取得したプロバイダ:"+provider);
-        }
-
-        //位置情報取得
-        mLocationManager.requestLocationUpdates(provider, 1, 1, this);
-
         //infoWindowのカスタマイズ
         mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
             @Override
@@ -128,6 +117,52 @@ public class MapsFragment extends Fragment implements LocationListener,OnMapRead
             }
         });
 
+        setUpLocationManager();
+
+        setMapListeners();
+
+        //スタンプラリーのスポット3点（デバッグ用）
+        oosakajo = mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(34.684581, 135.526349)).title("大阪城")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+        harukasu = mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(34.645842, 135.513971)).title("あべのハルカス").alpha(0.4f));
+        usj = mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(34.6671654, 135.4356473)).title("USJ"));
+
+    }
+
+    @Override   //位置座標を取得したら引数に渡して呼び出される
+    public void onLocationChanged(Location location) {
+        //カメラを現在地に移動
+        LatLng position = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(position));
+
+        //現在地から大阪城までの距離を計算（メートルで計算・WGS84楕円体）
+        float[] results = new float[1];
+        Location.distanceBetween(location.getLatitude(), location.getLongitude(), 34.684581, 135.526349, results);
+
+        //マーカーとの距離が50m以内の時にマーカーを切り替える
+        if(results[0] < CAN_STAMP_METER){
+            oosakajo.setIcon(nearMarker);
+        }else{
+            oosakajo.setIcon(defaultMarker);
+        }
+    }
+
+    private void setUpLocationManager(){
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);       //高精度
+        criteria.setAccuracy(Criteria.NO_REQUIREMENT);
+        String provider = mLocationManager.getBestProvider(criteria, true);
+        if(mLocationManager.isProviderEnabled(provider)){
+            System.out.println("取得したプロバイダ:"+provider);
+        }
+        //位置情報取得
+        mLocationManager.requestLocationUpdates(provider, 1, 1, this);
+    }
+
+    private void setMapListeners(){
         //マーカータップ時のイベントハンドラ（カメラボタンを表示する）
         mMap.setOnMarkerClickListener(new OnMarkerClickListener() {
             @Override
@@ -153,34 +188,6 @@ public class MapsFragment extends Fragment implements LocationListener,OnMapRead
             }
 
         });
-
-        //スタンプラリーのスポット3点（デバッグ用）
-        oosakajo = mMap.addMarker(new MarkerOptions()
-                .position(new LatLng(34.684581, 135.526349)).title("大阪城")
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
-        harukasu = mMap.addMarker(new MarkerOptions()
-                .position(new LatLng(34.645842, 135.513971)).title("あべのハルカス").alpha(0.4f));
-        usj = mMap.addMarker(new MarkerOptions()
-                .position(new LatLng(34.6671654, 135.4356473)).title("USJ"));
-
-    }
-
-    @Override   //位置座標を取得したら引数に渡して呼び出される
-    public void onLocationChanged(Location location) {
-        //カメラを現在地に移動
-        LatLng position = new LatLng(location.getLatitude(), location.getLongitude());
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(position));
-
-        //現在地から大阪城までの距離を計算（メートルで計算・WGS84楕円体）
-        float[] results = new float[1];
-        Location.distanceBetween(location.getLatitude(), location.getLongitude(), 34.684581, 135.526349, results);
-
-        //マーカーとの距離が50m以内の時にマーカーを切り替える
-        if(results[0] < 50){
-            oosakajo.setIcon(nearMarker);
-        }else{
-            oosakajo.setIcon(defaultMarker);
-        }
     }
 
     @Override
@@ -201,4 +208,10 @@ public class MapsFragment extends Fragment implements LocationListener,OnMapRead
         super.onStop();
         mLocationManager.removeUpdates(this);    // 位置情報の更新を止める
     }
+
+    @OnClick(R.id.cameraIcon)
+    void pushCameraIcon() {
+
+    }
+
 }
