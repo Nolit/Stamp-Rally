5095ac4b6256dc907e19218bc1f417eed1f607c0
Nolit <yaboonjapan@gmail.com>
2017-01-02 00:18:27 +0900

    スタンプ登録処理完成
    位置情報を取得するまで次の処理をさせない処理が必要になりそう

--- app/src/main/java/com/om1/stamp_rally/controller/TakeStampActivity.java
+++ app/src/main/java/com/om1/stamp_rally/controller/TakeStampActivity.java
@@ -1,6 +1,7 @@
 package com.om1.stamp_rally.controller;
 
 import android.content.Context;
+import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -27,28 +28,27 @@ public class TakeStampActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_camera);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        ButterKnife.inject(this);
 
-        //スタンプ取得時ではなくスタンプ登録時の処理
+        //スタンプ獲得時ではなく、スタンプ登録時には位置情報を取得する
         if(getIntent().getBooleanExtra("stampRegisterFlag", false) == true){
             requestLocationUpdates();
         }
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_camera);
+        ButterKnife.inject(this);
     }
 
     private void requestLocationUpdates(){
         //位置情報を取得してインテントに保存する処理
         mLocationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
         mLocationManager.requestLocationUpdates(
-                LocationManager.GPS_PROVIDER, //LocationManager.NETWORK_PROVIDER,
-                3000, // 通知のための最小時間間隔（ミリ秒）
-                10, // 通知のための最小距離間隔（メートル）
+                LocationManager.GPS_PROVIDER,
+                1000,
+                3,
                 new LocationListener() {
                     @Override
                     public void onLocationChanged(Location location) {
-                        Log.d("スタンプラリー", "位置情報を取得");
                         latitude = location.getLatitude();
                         longitude = location.getLongitude();
                         mLocationManager.removeUpdates(this);
@@ -68,6 +68,9 @@ public class TakeStampActivity extends AppCompatActivity {
 
     @OnClick(R.id.stamp_button)
     void pushStampButton(){
+        Intent intent = getIntent();
+        intent.putExtra("latitude", latitude);
+        intent.putExtra("longitude", longitude);
         EventBusUtil.defaultBus.post(getIntent());
     }
 }
