83961235b350d7c32b5bab651722e21caaeb50de
Nolit <yaboonjapan@gmail.com>
2017-01-03 16:59:50 +0900

    スタンプ編集ボタンの名前変更

--- app/src/main/java/com/om1/stamp_rally/controller/MainActivity.java
+++ app/src/main/java/com/om1/stamp_rally/controller/MainActivity.java
@@ -5,18 +5,14 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -143,7 +139,7 @@ public class MainActivity  extends FragmentActivity implements OnMapReadyCallbac
         });
 
         //スタンプ編集一覧ページへのインテントボタン
-        stampEditButton = (Button) findViewById(R.id.EditStamp);
+        stampEditButton = (Button) findViewById(R.id.editStamp);
         stampEditButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, StampEditListActivity.class);
