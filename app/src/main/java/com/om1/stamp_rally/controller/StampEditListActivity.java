0732d0837008e3137cc7dd72bda6cd4b4411eb6e
Nolit <yaboonjapan@gmail.com>
2017-01-07 11:52:15 +0900

    リストビューに画像を表示するサンプル作成

--- app/src/main/java/com/om1/stamp_rally/controller/StampEditListActivity.java
+++ app/src/main/java/com/om1/stamp_rally/controller/StampEditListActivity.java
@@ -1,5 +1,7 @@
 package com.om1.stamp_rally.controller;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
@@ -7,14 +9,11 @@ import android.widget.ListView;
 import com.om1.stamp_rally.R;
 import com.om1.stamp_rally.model.adapter.StampEditListAdapter;
 
-import org.greenrobot.eventbus.EventBus;
-
 import java.util.ArrayList;
 
 import com.om1.stamp_rally.model.adapter.StampEditListAdapter.StampData;
 
 public class StampEditListActivity extends AppCompatActivity {
-    private final EventBus eventBus = EventBus.getDefault();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +25,7 @@ public class StampEditListActivity extends AppCompatActivity {
 
         StampData stampData = adapter.createData();
         stampData.setTitle("hello-title");
+        stampData.setThumbnail(getImage());
         stampDataList.add(stampData);
 
         ListView listView = (ListView)findViewById(R.id.stampEditlistView);
@@ -36,5 +36,9 @@ public class StampEditListActivity extends AppCompatActivity {
     @Override
     public void onStop() {
         super.onStop();
+    }
+
+    private Bitmap getImage(){
+        return BitmapFactory.decodeResource(getResources(), R.drawable.common_full_open_on_phone);
     }
 }
