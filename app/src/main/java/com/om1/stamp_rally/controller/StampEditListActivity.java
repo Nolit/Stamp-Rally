c212a366238b40cbe595bb56dcbcb2b6e7f673fc
Nolit <yaboonjapan@gmail.com>
2017-01-07 22:23:37 +0900

    スタンプ編集ダイアログ表示処理作成

--- app/src/main/java/com/om1/stamp_rally/controller/StampEditListActivity.java
+++ app/src/main/java/com/om1/stamp_rally/controller/StampEditListActivity.java
@@ -3,29 +3,38 @@ package com.om1.stamp_rally.controller;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.om1.stamp_rally.R;
+import com.om1.stamp_rally.model.StampUpload;
 import com.om1.stamp_rally.model.adapter.StampEditListAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import com.om1.stamp_rally.utility.ByteConverter;
 import com.om1.stamp_rally.utility.dbadapter.StampDbAdapter;
 import com.om1.stamp_rally.utility.dbadapter.StampRallyDbAdapter;
 
 import database.entities.StampRallys;
 import database.entities.Stamps;
+
+import static butterknife.ButterKnife.findById;
 
 public class StampEditListActivity extends AppCompatActivity {
     private final String DIALOG_TITLE = "アップロードしますか";
@@ -36,6 +45,19 @@ public class StampEditListActivity extends AppCompatActivity {
     private final String UPLOAD_FAILE_MESSAGE = "アップロードに失敗しました\n編集画面からもう一度お試しください";
     private final String RALLY_COMPLETE_MESSAGE = "クリアしました！";
     private final String ATTENTION_STAMP_SAVE_MESSAGE = "スタンプを保存してください！";
+    private final float OVERLAY_ALPHA = 0.7f;
+
+    private int stampId;
+    private int stampRallyId;
+    private double latitude;
+    private double longitude;
+    private String title;
+    private String note;
+    private byte[] picture;
+
+    EditText titleEdit;
+    TextView stampTitleError;
+    EditText noteEdit;
 
     private StampEditListAdapter adapter;
 
@@ -58,10 +80,13 @@ public class StampEditListActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), stamp.getStampName() + " clicked",
                         Toast.LENGTH_LONG).show();
 
+                final View layout = StampEditListActivity.this.getLayoutInflater().inflate(R.layout.stamp_info,
+                        (ViewGroup)findViewById(R.id.layout_root));
 
+                initDialogViews(layout, stamp);
+                showEditStampDialog(layout, stamp);
             }
         });
-
     }
 
     private List<Stamps> loadStampData(){
@@ -100,33 +125,63 @@ public class StampEditListActivity extends AppCompatActivity {
         return stamp;
     }
 
-//    private void showEditStampDialog(final View layout){
-//        new AlertDialog.Builder(this)
-//                .setTitle(DIALOG_TITLE)
-//                .setView(layout)
-//                .setPositiveButton(OK_BUTTON_MESSAGE, new DialogInterface.OnClickListener() {
-//                    public void onClick(DialogInterface dialog, int which) {
-//                        String title = titleEdit.getText().toString();
-//                        if(title.equals("")){
-//                            stampTitleError.setText(ERROR_MESSAGE);
-//                            return;
-//                        }
-//                        applyDialogEditField();
-//                        uploadStamp();
-//                    }
-//                })
-//                .setNegativeButton(NO_BUTTON_MESSAGE, new DialogInterface.OnClickListener() {
-//                    @Override
-//                    public void onClick(DialogInterface dialog, int which) {
-//                        applyDialogEditField();
-//                        saveStamp();
-//                        startActivity(new Intent(StampPreviewActivity.this, MainActivity.class));
-//                    }
-//                })
-//                .setCancelable(false)
-//                .create().show();
-//    }
+    private void initDialogViews(View layout, Stamps stamp){
+        titleEdit = findById(layout, R.id.stampTitleEdit);
+        titleEdit.setText(stamp.getStampName());
+        noteEdit = findById(layout, R.id.stampNoteEdit);
+        noteEdit.setText(stamp.getStampComment());
+        stampTitleError = findById(layout, R.id.stampTitleError);
+    }
 
+    private void showEditStampDialog(final View layout, Stamps stamp){
+        new AlertDialog.Builder(this)
+                .setTitle(DIALOG_TITLE)
+                .setView(layout)
+                .setPositiveButton(OK_BUTTON_MESSAGE, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        String title = titleEdit.getText().toString();
+                        if(title.equals("")){
+                            stampTitleError.setText(ERROR_MESSAGE);
+                            return;
+                        }
+                        applyDialogEditField();
+                        uploadStamp();
+                    }
+                })
+                .setNegativeButton(NO_BUTTON_MESSAGE, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                })
+                .setCancelable(false)
+                .create().show();
+    }
+
+    private void applyDialogEditField(){
+        title = titleEdit.getText().toString();
+        note = noteEdit.getText().toString();
+    }
+
+    private void showOverlay(){
+        FrameLayout overlayLayout = findById(this, R.id.uploading_overlay);
+        overlayLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
+        overlayLayout.setAlpha(OVERLAY_ALPHA);
+    }
+
+    private void uploadStamp(){
+        showOverlay();
+        long createTime = System.currentTimeMillis();
+        SharedPreferences pref = getSharedPreferences("stamp-rally", MODE_WORLD_READABLE|MODE_WORLD_WRITEABLE);
+        String mailAddress = pref.getString("mailAddress", "tarou2");
+        String password = pref.getString("password", "tarou2");
+        StampUpload.getInstance().uploadStamp(stampId, stampRallyId, latitude, longitude, title, note, picture, createTime, mailAddress, password);
+    }
     @Override
     public void onStop() {
         super.onStop();
