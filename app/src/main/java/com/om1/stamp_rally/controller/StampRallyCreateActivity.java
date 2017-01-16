3dba860eef7e1c989d454a755c6b46319965c5c7
Nolit <yaboonjapan@gmail.com>
2017-01-17 07:44:27 +0900

    スタンプラリー作成機能完成

--- app/src/main/java/com/om1/stamp_rally/controller/StampRallyCreateActivity.java
+++ app/src/main/java/com/om1/stamp_rally/controller/StampRallyCreateActivity.java
@@ -9,13 +9,16 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.om1.stamp_rally.R;
 import com.om1.stamp_rally.model.CreatedStampRallyUploader;
+import com.om1.stamp_rally.model.MyStampBookModel;
 import com.om1.stamp_rally.model.adapter.MyStampBookListAdapter;
+import com.om1.stamp_rally.model.adapter.StructureStampListAdapter;
 import com.om1.stamp_rally.model.bean.StampBean;
 import com.om1.stamp_rally.model.event.FetchJsonEvent;
 import com.om1.stamp_rally.utility.EventBusUtil;
@@ -26,6 +29,7 @@ import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -57,6 +61,40 @@ public class StampRallyCreateActivity extends AppCompatActivity {
         stampRallyData = new StampRallyDbAdapter(this).getById(stampRallyId);
         editTitle.setText((String) stampRallyData.get("name"));
         editComment.setText((String) stampRallyData.get("summary"));
+
+        MyStampBookModel.getInstance().fetchJson(mainPref.getString("loginUserId", "20"));
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void fetchedJson(FetchJsonEvent event) {
+        if (!event.isSuccess()) {
+            Log.d("デバッグ:MyStampBook", "データベースとの通信に失敗");
+            return;
+        }
+        try {
+            Log.d("デバッグ:MyStampBook", "データベースとの通信に成功");
+            String[] responseData = event.getJson().split(System.getProperty("line.separator"));
+            StampData[] myStampBook = new ObjectMapper().readValue(responseData[1], StampData[].class);
+            ArrayList<StampBean> myStampList = new ArrayList<>();
+            List<Integer> idList = new StructureStampDbAdapter(this).getByStampRallyIdAsList(stampRallyId);
+            for(StampData stampData : myStampBook){
+                for(Integer id : idList){
+                    if(id.equals(stampData.getStampId())){
+                        StampBean stampBean = new StampBean();
+                        stampBean.setStampId(stampData.getStampId());
+                        stampBean.setPictPath(stampData.getPicture());
+                        stampBean.setStampTitle(stampData.getStampName());
+                        myStampList.add(stampBean);
+                        break;
+                    }
+                }
+            }
+            MyStampBookListAdapter adapter = new MyStampBookListAdapter(this, 0, myStampList);
+            ListView lv = (ListView) findViewById(R.id.stampInsertedListView);
+            lv.setAdapter(adapter);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 
     @OnClick(R.id.saveStampRallyButton)
