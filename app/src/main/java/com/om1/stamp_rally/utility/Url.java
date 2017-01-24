6e151abdd456e438e1f50ed03ec0867a567a3856
Nolit <yaboonjapan@gmail.com>
2017-01-24 19:11:13 +0900

    URLに設定を追加

--- app/src/main/java/com/om1/stamp_rally/utility/Url.java
+++ app/src/main/java/com/om1/stamp_rally/utility/Url.java
@@ -9,8 +9,9 @@ public final class Url {
 //    public static final String HOST = "35.160.83.150";    //AWS
 //    public static final String HOST = "localhost";
 //    public static final String HOST = "10.0.2.2";         //エミュレータ
+    public static final String HOST = "192.168.2.101";  //ルーター
 //    public static final String HOST = "192.168.137.1";  //水薮PC
-    public static final String HOST = "172.20.10.3";  //iphoneテザリング
+//    public static final String HOST = "172.20.10.3";  //iphoneテザリング
     //ポート番号
 //    public static final Integer PORT = 80;
     public static final Integer PORT = 8080;
